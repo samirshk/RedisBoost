4753cdec947c6afaa6741a24580fce7a5a336926
Samir Shaikh <s.shaikh@samsung.com>
2018-06-27 20:26:49 -0700

    [#55] support StackExchange Client

--- tests/RedisBoost.ConsoleBenchmark/Clients/ServiceStackTestClient.cs
+++ tests/RedisBoost.ConsoleBenchmark/Clients/ServiceStackTestClient.cs
@@ -1,90 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using ServiceStack.Redis.Pipeline;
+using System.Configuration;
 using System.Threading;
-using System.Threading.Tasks;
-using ServiceStack.Redis;
-using ServiceStack.Redis.Pipeline;
 
 namespace RedisBoost.ConsoleBenchmark.Clients
 {
-	public class ServiceStackTestClient: ITestClient
-	{
-		private ServiceStack.Redis.RedisClient _client;
-		private int _pipelineMode = 0;
-		private IRedisPipeline _pipeline;
-		public void Dispose()
-		{
-			if (_pipeline != null)
-				_pipeline.Dispose();
-			if (_client != null)
-				_client.Dispose();
-		}
+    public class ServiceStackTestClient : ITestClient
+    {
+        private ServiceStack.Redis.IRedisClient _client;
+        private int _pipelineMode = 0;
+        private IRedisPipeline _pipeline;
 
-		public string ClientName { get { return "servicestack"; } }
-		public void Connect(RedisConnectionStringBuilder connectionString)
-		{
-			
-			_client = new ServiceStack.Redis.RedisClient(((IPEndPoint)connectionString.EndPoint).Address.ToString(),
-														 ((IPEndPoint)connectionString.EndPoint).Port);
-		}
+        string _redisConfigurationString = ConfigurationManager.ConnectionStrings["RedisConfigurationString"].ConnectionString;
 
-		public void SetAsync(string key, string value)
-		{
-			EnterPipeline();
-			_pipeline.QueueCommand(c=>c.Set(key,value));
-		}
+        public void Dispose()
+        {
+            if (_pipeline != null)
+                _pipeline.Dispose();
+            if (_client != null)
+                _client.Dispose();
+        }
 
-		private void EnterPipeline()
-		{
-			if (Interlocked.CompareExchange(ref _pipelineMode, 1, 0) == 0)
-				_pipeline = _client.CreatePipeline();
-		}
+        public string ClientName { get { return "ServiceStack.Redis"; } }
+        public void Connect(RedisConnectionStringBuilder connectionString)
+        {
+            ServiceStack.Redis.RedisManagerPool redisManager = new ServiceStack.Redis.RedisManagerPool(_redisConfigurationString);
+            _client = redisManager.GetClient();
+        }
 
-		public void Set(string key, string value)
-		{
-			LeavePipelining();
-			_client.Set(key, value);
-		}
+        public void SetAsync(string key, string value)
+        {
+            EnterPipeline();
+            _pipeline.QueueCommand(c => c.Set(key, value));
+        }
 
-		private void LeavePipelining()
-		{
-			if (Interlocked.CompareExchange(ref _pipelineMode,0,1)==1)
-			{
-				_pipeline.Flush();
-				_pipeline.Dispose();
-			}
-		}
+        private void EnterPipeline()
+        {
+            if (Interlocked.CompareExchange(ref _pipelineMode, 1, 0) == 0)
+                _pipeline = _client.CreatePipeline();
+        }
 
-		public string GetString(string key)
-		{
-			LeavePipelining();
-			return _client.Get<string>(key);
-		}
+        public void Set(string key, string value)
+        {
+            LeavePipelining();
+            _client.Set(key, value);
+        }
 
-		public void FlushDb()
-		{
-			LeavePipelining();
-			_client.FlushDb();
-		}
+        private void LeavePipelining()
+        {
+            if (Interlocked.CompareExchange(ref _pipelineMode, 0, 1) == 1)
+            {
+                _pipeline.Flush();
+                _pipeline.Dispose();
+            }
+        }
 
-		public void IncrAsync(string KeyName)
-		{
-			EnterPipeline();
-			_pipeline.QueueCommand(c=>c.Increment(KeyName,1));
-		}
+        public string GetString(string key)
+        {
+            LeavePipelining();
+            return _client.Get<string>(key);
+        }
 
-		public int GetInt(string key)
-		{
-			LeavePipelining();
-			return _client.Get<int>(key);
-		}
+        public void FlushDb()
+        {
+            LeavePipelining();
+            _client.FlushDb();
+        }
 
-		public ITestClient CreateOne()
-		{
-			return new ServiceStackTestClient();
-		}
-	}
+        public void IncrAsync(string KeyName)
+        {
+            EnterPipeline();
+            _pipeline.QueueCommand(c => c.Increment(KeyName, 1));
+        }
+
+        public int GetInt(string key)
+        {
+            LeavePipelining();
+            return _client.Get<int>(key);
+        }
+
+        public ITestClient CreateOne()
+        {
+            return new ServiceStackTestClient();
+        }
+    }
 }
---
+++ tests/RedisBoost.ConsoleBenchmark/Clients/StackExchangeTestClient.cs
@@ -0,0 +1,71 @@
+﻿using StackExchange.Redis;
+using System.Configuration;
+using System.Net;
+
+namespace RedisBoost.ConsoleBenchmark.Clients
+{
+    public class StackExchangeTestsClient : ITestClient
+    {
+        private ConnectionMultiplexer _conn;
+        private IDatabase _client;
+        string RedisConnectionString = ConfigurationManager.ConnectionStrings["RedisConfigurationString"].ConnectionString;
+
+
+        public void Dispose()
+        {
+            if (_conn != null)
+                _conn.Dispose();
+        }
+
+        public void Connect(RedisConnectionStringBuilder connectionString)
+        {
+            _conn = ConnectionMultiplexer.Connect(RedisConnectionString);
+            //new RedisConnection(, allowAdmin: true);
+            //_client.Open();
+            _client = _conn.GetDatabase();
+        }
+
+        public void SetAsync(string key, string value)
+        {
+            _client.StringSetAsync(key, value);
+        }
+
+        public string GetString(string key)
+        {
+            return _client.StringGet(key);
+        }
+
+        public void FlushDb()
+        {
+            foreach (EndPoint e in _conn.GetEndPoints())
+            {
+                _conn.GetServer(e).FlushAllDatabasesAsync().Wait();
+            }
+        }
+
+        public string ClientName
+        {
+            get { return "StackExchange.Redis"; }
+        }
+
+        public void IncrAsync(string key)
+        {
+            _client.StringIncrementAsync(key);
+        }
+
+        public int GetInt(string key)
+        {
+            return (int)_client.StringGet(key);
+        }
+
+        public ITestClient CreateOne()
+        {
+            return new StackExchangeTestsClient();
+        }
+
+        public void Set(string key, string value)
+        {
+            _client.StringSet(key, value);
+        }
+    }
+}
